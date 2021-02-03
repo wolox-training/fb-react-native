d9c5840ed11444d6ce6f773e4080fad1117bb52a
NanduWx <fernando.badell@wolox.com.ar>
2021-02-03 15:39:18 -0300

    Add BookList

---
+++ wbooks/src/app/components/BookList/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 15
+  },
+  separator: {
+    height: 10
+  }
+});
+
+export default styles;
