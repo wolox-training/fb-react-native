c8eabb5c06b8dca0cc3eba8e95780dd8692974fb
NanduWx <fernando.badell@wolox.com.ar>
2021-03-02 13:34:45 -0300

    Login Layout

---
+++ wbooks/src/app/components/CustomInput/styles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import Colors from '@constants/colors';
+
+const styles = StyleSheet.create({
+  input: {
+    marginVertical: 7,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    backgroundColor: Colors.white,
+    borderRadius: 100
+  }
+});
+
+export default styles;
---
+++ wbooks/src/app/screens/Login/styles.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    paddingHorizontal: 30
+  },
+  logoContainer: {
+    flex: 0.6,
+    justifyContent: 'flex-end'
+  },
+  logo: {
+    alignSelf: 'center',
+    marginBottom: 80
+  },
+  formContainer: {
+    flex: 0.4,
+    justifyContent: 'center'
+  }
+});
+
+export default styles;
